f06e7e92fcf0aa7ec7948c83cad536fc7d421746
Hailey Webber <hjwebber97@gmail.com>
2023-01-31 14:14:15 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,63 @@
+console.log('May Node be with you')
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://haileyjocodes:Sophie0923!@cluster0.oek49b5.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-qotes')
+        const quotesCollection = db.collection('quotes')
+        app.set('view engine', 'ejs')
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(express.static('public'))
+        app.use(bodyParser.json())
+
+        app.get('/', (req, res) => {
+            quotesCollection.find().toArray()
+                .then(results => {
+                    console.log(results)
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(error => console.error(error))
+        })
+        app.post('/quotes', (req, res) => {
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
+        })
+        app.put('/quotes', (req, res) => {
+            quotesCollection.findOneAndUpdate(
+                { name: 'yoda ' },
+                {
+                    $set: {
+                        name: req.body.name,
+                        quote: req.body.quote
+                    }
+                },
+                {
+                    upsert: true
+                }
+            )
+                .then(result => {
+                    console.log(result)
+                })
+                .catch(error => console.error(error))
+        })
+        app.listen(3000, function () {
+            console.log('listening on 3000')
+        })
+    })
+    .catch(error => console.error(error))
+
+
+
+
+
+
